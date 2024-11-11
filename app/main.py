98a81c5449e547d973c77c1767a48b092bfea164
Fr4n05 <160607626+Fr4n05@users.noreply.github.com>
2024-11-11 19:09:52 +0000

    update

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from services.usuario_service import UsuarioService
+from repositories.usuario_repository import UsuarioRepository
+from config.connection import Session
+from models.usuario import Usuario
+import os 
+def main():
+    usuario = Usuario()
+    session = Session()
+    repository = UsuarioRepository(session)
+    service = UsuarioService(repository)
+
+
+    # Create dados do usuário.
+    print("\nAdicionando usuário: ")
+    nome = input("Digite o nome do usuário: ")
+    email = input("Digite o email do usuário: ")
+    senha = input("Digite a senha do usuário: ")
+    service.criar_usuario(nome=nome, email=email, senha=senha)
+
+    # Read dados do usuário sendo listado.
+    print("\nListando usuários cadastrados: ")
+    lista_usuarios = service.listar_todos_usuarios()
+    for usuario in lista_usuarios:
+        print(f"Nome: {usuario.nome} \nE-mail: {usuario.email} \nSenha: {usuario.senha}\n\n")
+
+
+if __name__ == "__main__":
+    os.system("cls || clear")
+    main()
